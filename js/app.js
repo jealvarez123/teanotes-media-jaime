69b35af90b9da17d0b387e06ec9e06bc2da40780
Fabian Socarras <fabian@advento.io>
2018-06-20 17:04:02 -0400

    Added base component

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
 $(function() {
-	var fabian = "Fabian was here.",
-			jamie = "";
-	console.log(fabian + " " + jamie)
+	// Sample FadeIn Animation
+	var media = $('.teanotes-interactive-media');
+
+	$('#toggleFadeIn').on('click', function(event) {
+		event.preventDefault();
+		toggleFadeIn(media);
+	});
+
+	function toggleFadeIn(el) {
+		if (el.hasClass('fadeIn')) {
+			el.removeClass('fadeIn')
+		} else {
+			el.addClass('fadeIn');
+			setTimeout(function() {
+				el.removeClass('fadeIn')	
+			}, 1000)
+		}
+	}
+
+
 })
