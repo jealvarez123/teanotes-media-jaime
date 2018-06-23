c5a33ac002b94a021583eff9705c2320715b66f7
jealvarez123 <jealvarez123@gmail.com>
2018-06-23 17:19:06 -0500

    Added fade in for all sample-pane

--- js/app.js
+++ js/app.js
@@ -1,17 +1,37 @@
 console.log('Can I kick it?');
 
 
-$(window).scroll(function() {
+// $(window).scroll(function() {
+//
+//     //Hide/Show based on location
+//     var y = $(this).scrollTop();
+//     if (y < 150) {
+//         $('.sample-pane-2').hide();
+//     }
+// 		else {
+// 				$('.sample-pane-2').fadeIn(200);
+//     }
+// });
 
-    //Hide/Show based on location
-    var y = $(this).scrollTop();
-    if (y < 150) {
-        $('.sample-pane-2').hide();
-    }
-		else {
-				$('.sample-pane-2').fadeIn(200);
-    }
-});
+$(window).scroll( function(){
+
+        /* Check the location of each desired element */
+        $('.sample-pane').each( function(i){
+
+            var bottom_of_object= $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'opacity':'1'},1500);
+
+            }
+
+        });
+
+    });
+
 
 
 $(function() {
