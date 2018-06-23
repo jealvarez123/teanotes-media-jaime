8d4a3d885eb067063d95ebe3dd4e104f69a14043
jealvarez123 <jealvarez123@gmail.com>
2018-06-23 16:52:55 -0500

    Added a test sample for the fade in one of the panels

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 console.log('Can I kick it?');
 
+
+$(window).scroll(function() {
+
+    //Hide/Show based on location
+    var y = $(this).scrollTop();
+    if (y < 150) {
+        $('.sample-pane-2').hide();
+    }
+		else {
+				$('.sample-pane-2').fadeIn(200);
+    }
+});
+
+
 $(function() {
 	// Sample FadeIn Animation
 	var media = $('.teanotes-interactive-media');
