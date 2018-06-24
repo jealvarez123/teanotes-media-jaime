d062b1a159898f9c81f0c3e397aa89ea3123904c
jealvarez123 <jealvarez123@gmail.com>
2018-06-24 17:15:44 -0500

    added the for all item descriptions. I will work on making an event for each one now

--- js/app.js
+++ js/app.js
@@ -1,45 +1,26 @@
-console.log('Can I kick it?');
-
+// This is for the scroll fade in
 $(window).scroll( function(){
+	/* Check the location of each desired element */
+	$('.sample-pane').each( function(i){
+		var bottom_of_object= $(this).position().top + $(this).outerHeight();
 
-        /* Check the location of each desired element */
-        $('.sample-pane').each( function(i){
+		var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            var bottom_of_object= $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-            /* If the object is completely visible in the window, fade it in */
-            if( bottom_of_window > bottom_of_object ){
-
-                $(this).animate({'opacity':'1'},1500);
-
-            }
-
-        });
-
-    });
-
-
-
-$(function() {
-	// Sample FadeIn Animation
-	var media = $('.teanotes-interactive-media');
-
-	$('#toggleFadeIn').on('click', function(event) {
-		event.preventDefault();
-		toggleFadeIn(media);
-	});
-
-	function toggleFadeIn(el) {
-		if (el.hasClass('fadeIn')) {
-			el.removeClass('fadeIn')
-		} else {
-			el.addClass('fadeIn');
-			setTimeout(function() {
-				el.removeClass('fadeIn')
-			}, 1000)
+		/* If the object is completely visible in the window, fade it in */
+		if( bottom_of_window > bottom_of_object ){
+			$(this).animate({'opacity':'1'},1500);
 		}
-	}
+	});
+});
 
-
+// This is the function that makes the text for the items appear
+$(function() {
+	$('.im-dot').on('click', function(event) {
+		console.log('it works');
+		$('.im-dot--meta').toggle(function () {
+			$("im-dot--meta").css({display: "block"});
+		}, function () {
+			$("im-dot--meta").css({display: "none"});
+		});
+	});
 })
